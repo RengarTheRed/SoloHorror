bc75f53aa55832da6ae6a9338d7bf1f51b3dab31
Dave <davidosborne3875@gmail.com>
2023-07-29 14:15:02 +0100

    Initial Project Commit
    Blank Project Created and Put on Repo

---
+++ Source/SoloHorror/SoloHorror.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "SoloHorror.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, SoloHorror, "SoloHorror" );
